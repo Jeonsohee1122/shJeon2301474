22c2341f63270e415f7e0c01dceae194b16ce43b
Jeonsohee1122 <wjsthgml040726@naver.com>
2023-04-17 01:47:55 +0900

    while버전추가

--- for_prac.py
+++ for_prac.py
@@ -15,6 +15,14 @@ for i in range(0, 3):
 # list에 입력받은 name 값 추가(append)
     name_list.append(name)
 
+# while 버전
+# i=0
+# while i<3:
+#     name = input("이름을 입력하세요: ")
+#     # list에 입력받은 name 값 추가(append)
+#     name_list.append(name)
+#     i = i+1
+
 # 오름차순: sort(), 내림차순: sort(reverse=True)
 name_list.sort(reverse=True)
 print(name_list)
